70436854cb0afa6e49981815dc49400d7cfb4e93
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-03-29 12:00:38 +0100

    fetch headlines from CNN

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
 export default {
   async asyncData({ app }) {
     const { articles } = await app.$axios.$get(
-      `https://newsapi.org/v2/top-headlines?country=ng&apiKey=${
+      `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${
         process.env.API_KEY
       }`
     );
